06f786d24a1a5423c06aa01d7ac4434646729d0b
antoniforzp <antonio25698@gmail.com>
2021-01-12 22:46:00 +0100

    Login screen, rest hooks connected, cors bypassed

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -9,15 +9,23 @@ import { LoginService } from './login.service';
 export class LoginComponent implements OnInit {
   // Login form storing input credentials
   loginForm = this.fb.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required],
+    email: ['admin@admin.com', Validators.required],
+    password: ['Password', Validators.required],
   });
 
   constructor(private fb: FormBuilder, private login: LoginService) {}
 
+  get email(): string {
+    return this.loginForm.get('email')?.value;
+  }
+
+  get password(): string {
+    return this.loginForm.get('password')?.value;
+  }
+
   ngOnInit(): void {}
 
   onSubmit(): void {
-    this.login.login();
+    this.login.login(this.email, this.password);
   }
 }
--- web/src/app/pages/login/login.service.ts
+++ web/src/app/pages/login/login.service.ts
@@ -1,12 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor() {}
+  loginUrl = 'http://localhost:8080/login/login';
 
-  public login(): void {
-    console.log('token');
+  constructor(private http: HttpClient) {}
+
+  public login(email: string, password: string): void {
+    const myHeaders = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+    const myBody = JSON.stringify({email, password});
+    this.http.post(this.loginUrl, myBody, { headers: myHeaders }).subscribe((x) => console.log(x));
   }
 }
