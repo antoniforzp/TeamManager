cfc8cd647faf520d77e5227dd30e8e29a18450e3
antoniforzp <antonio25698@gmail.com>
2021-05-24 20:46:36 +0200

    App title service

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -14,25 +15,27 @@ import { defaultLanguage, languages } from './translation/translation-config';
 export class AppComponent implements OnInit, OnDestroy {
   $destroy = new Subject();
 
-  title = 'Menadżer Drużyny Harcerskiej';
-
   constructor(
     private translate: TranslateService,
+    private titleService: Title,
     private router: Router,
     private appStateSerivce: AppStateService,
     private appThemeService: AppThemeService
   ) {
+    this.initLanguage();
     this.translate.addLangs(languages);
+
+    this.titleService.setTitle('Scout Team Manager');
 
     // Keep application state on refresh
     this.router.events.pipe(takeUntil(this.$destroy)).subscribe({
-      next: () => {
-        const lang = this.appStateSerivce.getLanguage();
+      next: (x) => {
+        const lang = this.appStateSerivce.language;
         if (lang) {
           this.translate.use(lang);
         }
 
-        const themeId = this.appStateSerivce.getTheme();
+        const themeId = this.appStateSerivce.theme;
         if (themeId) {
           this.appThemeService.setThemeById(themeId);
         }
@@ -41,11 +44,15 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.translate.setDefaultLang(defaultLanguage);
-    this.translate.use(defaultLanguage);
+    this.initLanguage();
   }
 
   ngOnDestroy(): void {
     this.$destroy.next();
+  }
+
+  private initLanguage(): void {
+    this.translate.setDefaultLang(defaultLanguage);
+    this.translate.use(defaultLanguage);
   }
 }
