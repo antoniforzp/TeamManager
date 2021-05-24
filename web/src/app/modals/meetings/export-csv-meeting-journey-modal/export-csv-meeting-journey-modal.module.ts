6e31b444a838d38d1e77d4698957386b532a1675
antoniforzp <antonio25698@gmail.com>
2021-05-24 20:23:20 +0200

    Export meetings to csv translated

--- web/src/app/modals/meetings/export-csv-meeting-journey-modal/export-csv-meeting-journey-modal.module.ts
+++ web/src/app/modals/meetings/export-csv-meeting-journey-modal/export-csv-meeting-journey-modal.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/material/app-material.module';
 import { ExportCsvMeetingJourneyModalComponent } from './export-csv-meeting-journey-modal.component';
+import { AppTranslationModule } from 'src/app/translation/app-translation.module';
 
 @NgModule({
   declarations: [ExportCsvMeetingJourneyModalComponent],
@@ -13,6 +14,7 @@ import { ExportCsvMeetingJourneyModalComponent } from './export-csv-meeting-jour
     ReactiveFormsModule,
     MaterialModule,
     SharedModule,
+    AppTranslationModule,
   ],
 })
 export class ExportCsvScoutModalModule {}
