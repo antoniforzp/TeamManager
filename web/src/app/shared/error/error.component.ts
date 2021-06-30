7d2aa2a7b7c82d00707cb9269d3eb9a026bcb35a
antoniforzp <antonio25698@gmail.com>
2021-06-30 22:33:42 +0200

    Error handling fixed annd enhanced. Settings fixed, dark theme removed

--- web/src/app/shared/error/error.component.ts
+++ web/src/app/shared/error/error.component.ts
@@ -2,6 +2,12 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Input, OnChanges } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
+enum ErrorTypes {
+  GENERIC,
+  SERVER_ERROR,
+  LOST_CONNECTION,
+}
+
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
@@ -9,6 +15,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit, OnChanges {
   @Input() error: HttpErrorResponse | Error;
+
+  ErrorTypes = ErrorTypes;
+  errorType: ErrorTypes;
 
   errorName: string;
   errorMessage: string;
@@ -20,5 +29,15 @@ export class ErrorComponent implements OnInit, OnChanges {
   ngOnChanges(): void {
     this.errorName = this.error.name;
     this.errorMessage = this.error.message;
+
+    if (this.error instanceof HttpErrorResponse) {
+      if (this.error.status === 0) {
+        this.errorType = ErrorTypes.LOST_CONNECTION;
+      } else if (this.error.status === 500) {
+        this.errorType = ErrorTypes.SERVER_ERROR;
+      } else {
+        this.errorType = ErrorTypes.GENERIC;
+      }
+    }
   }
 }
